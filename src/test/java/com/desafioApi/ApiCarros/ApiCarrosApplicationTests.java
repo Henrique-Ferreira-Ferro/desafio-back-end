879aacd662517331e589f320ac27a4de84cd565d
Henrique Ferreira Ferro <henriqueferreiraferro@gmail.com>
2024-04-18 19:18:42 -0300

    inicio do desafio

---
+++ src/test/java/com/desafioApi/ApiCarros/ApiCarrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafioApi.ApiCarros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCarrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
